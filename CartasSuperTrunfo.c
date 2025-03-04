b6923c4af99b64d1517bca3e319b160de1575b18
Jpmaster-creator <joaopedrodsn20@gmail.com>
2025-03-04 06:36:31 +0000

    Iniciei

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,37 @@
 //Teste larissa
 
 int main() {
+    char estado;
+    char codigo_de_carta [20];
+    char nome_da_cidade [58];
+    int população;
+    float Área;
+    float PIB;
+    int Quantidade_de_Pontos_Turísticos;
+
+
+    printf("Por favor, digite a letra inicial do estado de sua escolha: ");
+    scanf("%c", &estado);
+
+    printf("Agora insira a letra do estado seguida de um número de 01 a 04 (ex: A01, B03):");
+    scanf("%s", &codigo_de_carta);
+
+    printf("Digite o nome da cidade escolhida: ");
+    scanf("%s", &nome_da_cidade);
+
+    printf("Defina O número de habitantes da cidade: ");
+    scanf("%d", &população);
+
+    printf("Defina agora o tamanho da área da cidade: ");
+    scanf("%f", &Área);
+
+    printf("Escolha o PIB, referente a cidade: ");
+    scanf("%d", &Quantidade_de_Pontos_Turísticos);
+
+
+    sprintf("Sua carta está pronta!");
+    sprintf("Carta 1"\n%c, estado);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
